ab9eaa6def7cd64afb1839bbecc2567667e34c44
Rima <rmhrok@gmail.com>
2018-03-06 21:16:26 +0000

    add big O

--- whiteboard-challenge-37/lib/solution.js
+++ whiteboard-challenge-37/lib/solution.js
@@ -5,6 +5,7 @@ const BT = require('./bt');
 const BST = require('./bst');
 const SLL = require('./sll');
 
+// O(n) where n is the number of nodes in input argument 'bt'
 function btToSortedSll(bt){
   if(!(bt instanceof BT)){
     throw new Error('Input is not instaceof Binary Tree');
